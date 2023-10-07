6d0eb2c3cc1e0f1f8eee0d5d90abf3b08f387f3a
SiddharthPalod <75942243+SiddharthPalod@users.noreply.github.com>
2023-10-07 23:47:10 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Intropg1, About , Page1, Page2, Page3, Navbar1} from './components'
+
+
+function App() {
+
+  return (
+    <main className="app transition-all ease-in">
+      <Navbar1/>
+      <Intropg1/>
+      <About/>
+    </main>
+  )
+}
+
+export default App
